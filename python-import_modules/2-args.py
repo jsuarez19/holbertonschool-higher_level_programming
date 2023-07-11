de1df252b798a352773afe8a06568cf8c86414e8
Jose Suarez <jolusuvi@gmail.com>
2023-07-11 16:38:20 -0700

    File uploading...

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
-    argc = len(sys.argv)
+    from sys import argv
+    argc = len(argv)
     if argc == 1:
         print("0 arguments.")
     elif argc != 1:
         print("{:d} argument".format(argc - 1), end="")
-        if argc == 2:
+        if argc > 2:
             print("s", end="")
         print(":")
         for i in range(1, argc):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
