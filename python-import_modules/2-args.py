bb4dfe6009e17fff9ce45fff8dab1f8886975c1f
Jose Suarez <jolusuvi@gmail.com>
2023-07-11 14:45:19 -0700

    File uploading...

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
             print("s", end="")
         print(":")
         for i in range(1, argc):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, sys.argv[i]))
