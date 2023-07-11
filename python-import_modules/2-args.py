62c7b6dd3435ee12e56981350acdec6701b62f54
Jose Suarez <jolusuvi@gmail.com>
2023-07-11 14:44:05 -0700

    File uploading...

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv)
-if argc == 1:
-    print("0 arguments.")
-elif argc != 1:
-    print("{:d} argument".format(argc - 1), end="")
-    if argc == 2:
-        print("s", end="")
-    print(":")
-    for i in range(1, argc):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == '__main__':
+    import sys
+    argc = len(sys.argv)
+    if argc == 1:
+        print("0 arguments.")
+    elif argc != 1:
+        print("{:d} argument".format(argc - 1), end="")
+        if argc == 2:
+            print("s", end="")
+        print(":")
+        for i in range(1, argc):
+            print("{}: {}".format(i, sys.argv[i]))
