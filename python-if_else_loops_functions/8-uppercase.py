a865ee02160832aad034905112866a1372827421
Jose Suarez <jolusuvi@gmail.com>
2023-07-10 19:17:52 -0700

    File uploading...

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 64 < ord(char) < 91:
+            print("{}", end="").format(chr(char + 32))
+        else:
+            print("{}", end="").format(chr(char))
