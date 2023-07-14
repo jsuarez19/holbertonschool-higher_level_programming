9542b5b18c555c130e458596119699dc7326ff67
Jose Suarez <jolusuvi@gmail.com>
2023-07-14 13:26:49 -0700

    File uploading...

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,8 +5,8 @@ def new_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return my_list
     else:
-        new_list[len(my_list)]
+        new_list = []
         for i in range(len(my_list)):
-            new_list[i] = my_list[i]
+            new_list.append(my_list[i])
         new_list[idx] = element
         return new_list
