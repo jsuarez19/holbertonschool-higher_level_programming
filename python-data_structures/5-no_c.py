0b85555e55e118c25a3acfcab229e435f276ee46
Jose Suarez <jolusuvi@gmail.com>
2023-07-14 13:55:36 -0700

    File uploading...

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = []
+    new_string = ''
     for letter in my_string:
-        if letter is not c or C:
-            new_string.append(letter)
+        if letter is not 'c' or 'C':
+            new_string += letter
     return new_string
 
