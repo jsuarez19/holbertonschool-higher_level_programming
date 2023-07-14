155d0259bf3799c41bef6961ceaea4dc2ca901da
Jose Suarez <jolusuvi@gmail.com>
2023-07-14 06:50:37 -0700

    File uploading...

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if __name__ == '__main__':
-        for element in my_list:
-            print("{:d}".format(element))
+    for element in my_list:
+        print("{:d}".format(element))
