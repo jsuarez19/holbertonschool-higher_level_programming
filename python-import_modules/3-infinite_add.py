41b5016dd2dee4f377f37d2f16d0866be3734bab
Jose Suarez <jolusuvi@gmail.com>
2023-07-14 12:43:59 -0700

    File uploading...

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == '__main__':
     from sys import argv
     argc = len(argv)
     inf_add = 0
-    for i in range(1,argc):
+    for i in range(1, argc):
         inf_add += int(argv[i])
     print("{:d}".format(inf_add))
