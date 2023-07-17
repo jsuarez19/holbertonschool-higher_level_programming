fd8f194d5e865e89d245aa784f60bf3d684c9996
Jose Suarez <jolusuvi@gmail.com>
2023-07-16 21:56:48 -0700

    File uploading...

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    for i, x in my_list:
+    for x in my_list:
         if x % 2 == 0:
             new_list.append(True)
         else:
