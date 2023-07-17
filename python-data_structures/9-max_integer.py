fed68a0470a9df536c3f39dc6d4a7226bc687ca3
Jose Suarez <jolusuvi@gmail.com>
2023-07-16 21:49:35 -0700

    File uploading...

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def max_integer(my_list=[]):
-        size = len(my_list)
-        bigger = 0
-        if size == 0:
-            return None
-        else:
-            for element in my_list:
-                if element > bigger:
-                    bigger = element
-            return bigger
+def max_integer(my_list=[]):
+    size = len(my_list)
+    bigger = 0
+    if size == 0:
+        return None
+    else:
+        for element in my_list:
+            if element > bigger:
+                bigger = element
+        return bigger
