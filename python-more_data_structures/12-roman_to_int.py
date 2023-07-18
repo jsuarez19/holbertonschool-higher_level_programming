c0743665d8cefd7e5b3f88193ff0899734f17a3f
Jose Suarez <jolusuvi@gmail.com>
2023-07-18 11:58:56 -0700

    File uploading...

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string is None:
+        return result
+    for i in range(len(roman_string) - 1, -1, -1):
+        roman_string[i] = k
+        v = dic[k]
+        if v >= aux:
+            result += v
+        else:
+            result = result - v
+        aux = v
+
+    return result
