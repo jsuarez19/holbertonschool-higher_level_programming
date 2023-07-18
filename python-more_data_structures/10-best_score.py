da32e5c0203c683e18dcf658608758614e7e6d5d
Jose Suarez <jolusuvi@gmail.com>
2023-07-18 11:31:28 -0700

    File uploading...

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     biggest = 0
     biggestKey = None
-    if a_dictionary == None:
+    if a_dictionary is None:
         return biggestKey
     for k, v in a_dictionary.items():
         if v > biggest:
