f0a1de4d490976bfc227e9aba812fff23bd07885
Jose Suarez <jolusuvi@gmail.com>
2023-07-18 12:00:24 -0700

    File uploading...

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     result = 0
+    aux = 0
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None:
         return result
