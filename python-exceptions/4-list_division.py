fca714a1fcfc1aa8d933e36a43577559ebbca1e3
Jose Suarez <jolusuvi@gmail.com>
2023-07-18 20:17:09 -0700

    File uploading...

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            new_list.append(a / b)
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+    finally:
+        return new_list
