520e1786a5765f9fa6e755bdbdae619d57e19400
Jose Suarez <jolusuvi@gmail.com>
2023-07-18 19:34:29 -0700

    File uploading...

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        i = 0
         for i in range(x):
             print("{}".format(my_list[i]), end='')
+    except IndexError:
+        break
+    finally:
         print()
         return i + 1
-    except IndexError:
-        pass
