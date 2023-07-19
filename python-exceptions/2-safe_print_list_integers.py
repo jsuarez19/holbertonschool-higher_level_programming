da192471dd6500da490a72dbb7f85d29851d8ab6
Jose Suarez <jolusuvi@gmail.com>
2023-07-18 19:51:41 -0700

    File uploading...

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    for element in my_list:
+        aux = 0
+        try:
+            print("{:d}".format(element))
+            aux += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return aux
