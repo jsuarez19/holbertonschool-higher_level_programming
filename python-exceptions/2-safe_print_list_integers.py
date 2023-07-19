c550650967b904ed9680e39a70a6d0f011ddc4e4
Jose Suarez <jolusuvi@gmail.com>
2023-07-18 19:52:18 -0700

    File uploading...

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for element in my_list:
         aux = 0
         try:
-            print("{:d}".format(element))
+            print("{:d}".format(element), end='')
             aux += 1
         except (TypeError, ValueError):
             pass
