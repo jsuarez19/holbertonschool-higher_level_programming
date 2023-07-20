e11352264adcdc0cafae52f8301e5011a02c0ec0
Jose Suarez <jolusuvi@gmail.com>
2023-07-19 20:12:50 -0700

    File uploading...

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""2. Size validation"""
+class Square:
+    """A class Square"""
+    def __init__(self, size=0):
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except TypeError:
+            print("size must be an integer")
