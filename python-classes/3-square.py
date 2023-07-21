dd0b1d8bf7d7ea07899f72d7a4c9b7d947894490
Jose Suarez <jolusuvi@gmail.com>
2023-07-20 17:21:42 -0700

    File uploading...

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Area of a square
+"""
+
+
+class Square:
+    """Create Square class"""
+
+    def __init__(self, size=0):
+        """Instantiation of Square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns area of square"""
+        
+        return size ** 2
