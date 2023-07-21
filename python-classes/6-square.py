18b51979f146d63a9f3e0a498c4d332b1ff19f79
Jose Suarez <jolusuvi@gmail.com>
2023-07-20 17:55:08 -0700

    File uploading...

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,7 +45,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print(self.__position[1] * '\n')
+            print(self.__position[1] * '')
             for i in range(self.__size):
                 print(self.__position[0] * ' ', end='')
                 for j in range(self.size):
