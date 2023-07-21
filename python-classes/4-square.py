3fc1ca0ea25c902a65805e9de75dbe9076238374
Jose Suarez <jolusuvi@gmail.com>
2023-07-20 17:32:35 -0700

    File uploading...

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,19 +7,20 @@ Access and update private attribute
 class Square:
     """Create Square class"""
 
+    def __init__(self, size=0):
+        self.__size = size
+
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def __init__(self, size=0):
-        """Instantiation of Square"""
-
-        if not isinstance(size, int):
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Returns area of square"""
