cb52b982f257223d0215070d0469575a1b2e69f5
Jose Suarez <jolusuvi@gmail.com>
2023-07-20 17:45:13 -0700

    File uploading...

---
+++ python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Printing a square
+"""
+
+
+class Square:
+    """Create Square class"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if position[0] < 0 and position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+    
+
+    def area(self):
+        """Returns area of square"""
+
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            print(position[1] * '\n')
+            for i in range(self.__size):
+                print(position[0] * ' ', end='')
+                for j in range(self.size):
+                    print("#", end='')
+                print()
