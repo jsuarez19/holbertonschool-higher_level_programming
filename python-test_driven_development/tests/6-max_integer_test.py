c002c2d8b01f17c618871b4e7adb4dfbeae9562c
Jose Suarez <jolusuvi@gmail.com>
2023-07-23 21:04:58 -0700

    File uploading...

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,5 +11,5 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(result, 3)
 
     def test_list_str(self):
-        self.assertRaises(TypeError, max_integer, ['a', 'n'])
+        self.assertRaises(TypeError, max_integer, ['a', 6])
 
