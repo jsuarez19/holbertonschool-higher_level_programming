aae48f87d5e8d4432af58ba16ce6b19ed36a2782
Jose Suarez <jolusuvi@gmail.com>
2023-07-23 19:36:48 -0700

    File uploading...

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Text indentation
+"""
+
+
+def text_indentation(text):
+    """adds 2 lines after some characters"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    newText = ""
+    symbols = [".", "?", ":"]
+    for char in text:
+        newText += char
+        if char in symbols:
+            newText += "\n\n"
+    print (newText)
