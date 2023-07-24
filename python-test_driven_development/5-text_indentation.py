0e86265d13cb01026e23424edc9c2175392463a4
Jose Suarez <jolusuvi@gmail.com>
2023-07-23 19:42:20 -0700

    File uploading...

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -17,4 +17,4 @@ def text_indentation(text):
         if char in symbols:
             newText = newText.strip()
             newText += "\n\n"
-    print (newText, end='')
+    print(newText, end='')
