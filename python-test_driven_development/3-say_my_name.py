2f93ec02fb528e7ffd7f531cb94d68419dbab57e
Jose Suarez <jolusuvi@gmail.com>
2023-07-23 18:53:33 -0700

    File uploading...

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Say my name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that prints name"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    elif last_name != "":
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
