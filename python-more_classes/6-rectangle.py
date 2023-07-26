42b75e3fd9fbe9dd5b18fae8fc98ac2e18a9a50c
Jose Suarez <jolusuvi@gmail.com>
2023-07-25 21:49:53 -0700

    File uploading...

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,7 +7,7 @@ Area and Perimeter
 class Rectangle:
     """Creates new class Rectangle"""
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
