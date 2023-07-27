9536c2fcd9779d00f643dfb0a79a7a0de2852cb5
Jose Suarez <jolusuvi@gmail.com>
2023-07-27 12:52:39 -0700

    File uploading...

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Only sub class of
+"""
+
+
+def inherits_from(obj, a_class):
+    """object is an instance of a class that inherited?"""
+    return issubclass(type(obj), a_class)
