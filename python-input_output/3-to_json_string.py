d2cb86c5aef310a3a9dea3af75504572ccb15fde
Jose Suarez <jolusuvi@gmail.com>
2023-07-30 13:47:50 -0700

    File uploading...

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+To JSON string
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    json_string = json.dumps(my_obj)
+    return json_string
