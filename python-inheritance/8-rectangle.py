1e52cb8aa8d1eb9e33b3288683b4f9c39f9eeede
Jose Suarez <jolusuvi@gmail.com>
2023-07-29 20:58:40 -0700

    File uploading...

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,8 +7,8 @@ Improve Geometry
 class BaseGeometry:
     """empty class"""
     def __init__(self, width, height):
-        integer_validator(self.__width, width)
-        integer_validator(self.__height, height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
