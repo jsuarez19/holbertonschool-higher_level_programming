758cdcee914d593342d6936be84d4e9fe0a3c4dd
Jose Suarez <jolusuvi@gmail.com>
2023-07-30 16:07:26 -0700

    File uploading...

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+"""
+Load, add, save
+"""
+
+
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+json_obj = load_from_json_file(filename)
+json_obj.extend(sys.argv[1:])
+save_to_json_file(json_obj, filename)
