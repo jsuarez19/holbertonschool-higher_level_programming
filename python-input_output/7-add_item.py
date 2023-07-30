2e85a94520a983543bb2136cf2685a4480f2f416
Jose Suarez <jolusuvi@gmail.com>
2023-07-30 16:11:17 -0700

    File uploading...

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ Load, add, save
 
 
 import json
+import os
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -12,6 +13,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
-json_obj = load_from_json_file(filename)
+if os.path.exists(filename):
+    json_obj = load_from_json_file(filename)
+else:
+    json_obj = []
 json_obj.extend(sys.argv[1:])
 save_to_json_file(json_obj, filename)
