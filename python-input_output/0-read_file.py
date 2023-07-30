057a40374df36a94295b73a92c79c215463c391b
Jose Suarez <jolusuvi@gmail.com>
2023-07-29 21:44:18 -0700

    File uploading...

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,5 +8,4 @@ def read_file(filename=""):
     """Reads a file using with"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-    print (read_data, end='')
-
+    print(read_data, end='')
