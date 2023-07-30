986cc229064c7776b011ba42071bfb3b206ce0ec
Jose Suarez <jolusuvi@gmail.com>
2023-07-30 13:09:57 -0700

    File uploading...

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ Write to a file
 
 def write_file(filename="", text=""):
     """Reads a file using with"""
-    with open(filename, mode='w' ,encoding="utf-8") as f:
+    with open(filename, mode='w', encoding="utf-8") as f:
         f.write(text)
