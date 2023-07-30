d10abccae1ecdde923f3868a02184c7ab3e4aa03
Jose Suarez <jolusuvi@gmail.com>
2023-07-30 13:18:00 -0700

    File uploading...

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Append to a file
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a file using with"""
+    with open(filename, mode='a', encoding="utf-8") as f:
+        f.append(text)
+    return len(text)
