233f6b9e3a0f9e55e2042002818d32d06aec753a
Jose Suarez <jolusuvi@gmail.com>
2023-07-30 17:21:33 -0700

    File uploading...

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        pascal = [[1]]
+        
+        for i in range(1, n):
+            row = []
+            row.append(1)
+            for j in range(1, i):
+                row.append(prev[j - 1] + prev[j])
+
+            row.append(1)
+            pascal.append(row)
+            prev = row
