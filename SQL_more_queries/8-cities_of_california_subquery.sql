404a5a8a4b4f5e963a1789547135a938a70f1f07
Jose Suarez <jolusuvi@gmail.com>
2023-08-26 18:44:16 -0500

    Placed order by into the subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,5 @@
 -- linking two ids
 SELECT id, name 
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California');
-
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id;
