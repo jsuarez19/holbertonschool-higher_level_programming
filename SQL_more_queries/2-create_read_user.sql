bfc1118f14ceea739601ac4b7dc3bd5ed2a9cbe9
Jose Suarez <jolusuvi@gmail.com>
2023-08-26 17:51:59 -0500

    Condition fixed

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,8 +2,8 @@
 -- give only READ privileges to the new user
 CREATE DATABASE hbtn_0d_2 IF NOT EXISTS;
 
-CREATE USER 'user_0d_2'@'localhost' 
-IDENTIFIED BY 'user_0d_2_pwd' IF NOT EXISTS;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' 
+IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT
 ON hbtn_0d_2.*
