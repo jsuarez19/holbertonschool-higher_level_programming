84a0a60b661c6000c4990d2854019528afd88816
Jose Suarez <jolusuvi@gmail.com>
2023-08-27 15:25:09 -0500

    File documented

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,21 @@ lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
+# Make a connection with the database
 db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+# Creating cursor
 c = db.cursor()
 
+# Execute queries
 query = "SELECT * FROM states ORDER BY id"
 c.execute(query)
 
+# Printing all the rows
 rows = c.fetchall()
 for row in rows:
     print(row)
 
+# Cleaning process
 c.close()
 db.close()
