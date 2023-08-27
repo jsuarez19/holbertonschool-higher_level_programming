556ab586cf55a6fcd2bc2fc8b373ad11f11581c3
Jose Suarez <jolusuvi@gmail.com>
2023-08-27 15:34:50 -0500

    To not execute when import

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,22 @@
 import MySQLdb
 import sys
 
-# Make a connection with the database
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ = '__name__':
+    # Make a connection with the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-# Creating cursor
-c = db.cursor()
+    # Creating cursor
+    c = db.cursor()
 
-# Execute queries
-query = "SELECT * FROM states ORDER BY id"
-c.execute(query)
+    # Execute queries
+    query = "SELECT * FROM states ORDER BY id"
+    c.execute(query)
 
-# Printing all the rows
-rows = c.fetchall()
-for row in rows:
-    print(row)
+    # Printing all the rows
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
 
-# Cleaning process
-c.close()
-db.close()
+    # Cleaning process
+    c.close()
+    db.close()
