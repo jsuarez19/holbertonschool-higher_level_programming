2829e7fc21654bf347b52848ab9038e6cb6474b3
Jose Suarez <jolusuvi@gmail.com>
2023-08-27 15:38:03 -0500

    Changing # to """

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,22 +3,24 @@
 import MySQLdb
 import sys
 
+""" To not execute when imported """
 if __name__ = '__name__':
-    # Make a connection with the database
+
+    """ Make a connection with the database """ 
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    # Creating cursor
+    """ Creating cursor """
     c = db.cursor()
 
-    # Execute queries
+    """ Execute queries """
     query = "SELECT * FROM states ORDER BY id"
     c.execute(query)
 
-    # Printing all the rows
+    """ Printing all the rows """
     rows = c.fetchall()
     for row in rows:
         print(row)
 
-    # Cleaning process
+    """ Cleaning process """
     c.close()
     db.close()
