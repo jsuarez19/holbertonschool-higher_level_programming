41f14aed7175aa3cce1e658389f198e657eb745e
Jose Suarez <jolusuvi@gmail.com>
2023-08-27 15:39:51 -0500

    Fixing typo

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 """ To not execute when imported """
-if __name__ = '__name__':
+if __name__ == '__main__':
 
     """ Make a connection with the database """ 
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
