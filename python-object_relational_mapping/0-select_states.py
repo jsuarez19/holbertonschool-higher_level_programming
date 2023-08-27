6f35cf64d31a1a5e41c525375826877546bfdd4d
Jose Suarez <jolusuvi@gmail.com>
2023-08-27 15:48:42 -0500

    Fixing pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,11 @@ import sys
 """ To not execute when imported """
 if __name__ == '__main__':
 
-    """ Make a connection with the database """ 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    """ Make a connection with the database """
+    db = MySQLdb.connect(host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
 
     """ Creating cursor """
     c = db.cursor()
@@ -20,7 +23,6 @@ if __name__ == '__main__':
     rows = c.fetchall()
     for row in rows:
         print(row)
-
     """ Cleaning process """
     c.close()
     db.close()
