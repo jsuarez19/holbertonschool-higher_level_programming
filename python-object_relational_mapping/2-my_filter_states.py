132b7f3055875810aa70dde634ca7c16050956e7
Jose Suarez <jolusuvi@gmail.com>
2023-08-27 21:36:02 -0500

    To filter by state name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. """
+import MySQLdb
+import sys
+
+""" To not execute when imported """
+if __name__ == '__main__':
+
+    """ Make a connection with the database """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    """ Creating cursor """
+    c = db.cursor()
+
+    """ Execute queries """
+    query = "SELECT * FROM states WHERE name = {:s} ORDER BY id".format(sys.argv[4])
+    c.execute(query)
+
+    """ Printing all the rows """
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    """ Cleaning process """
+    c.close()
+    db.close()
