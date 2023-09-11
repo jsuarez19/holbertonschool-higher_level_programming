7fc0f4f6211ad362b9b8343b3fcc9d3b10cd52f5
Jose Suarez <jolusuvi@gmail.com>
2023-09-11 17:39:14 -0500

    Fixing pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,20 +8,23 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 
-user=sys.argv[1]
-passwd=sys.argv[2]
-db=sys.argv[3]
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
 db_url = "mysql://{}:{}@localhost:3306/{}".format(user, passwd, db)
 
 engine = create_engine(db_url)
 Base = declarative_base()
+
 
 class State(Base):
     """"Creates State class, inherits from Base"""
 
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                 nullable=False, unique=True)
     name = Column(String(128), nullable=False)
+
 
 Base.metadata.create_all(engine)
