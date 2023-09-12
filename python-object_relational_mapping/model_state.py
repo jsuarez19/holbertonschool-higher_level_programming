5f16d70e0850981833da52048e341cb27baad1c9
Jose Suarez <jolusuvi@gmail.com>
2023-09-11 20:24:39 -0500

    remove lines

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,16 +4,10 @@ This module contains the class definition of a State including id and name
 Contains the State class that inherits from Base = declarative_base()
 """
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
-db_url = "mysql://{}:{}@localhost:3306/{}".format(user, passwd, db)
 
-engine = create_engine(db_url)
 Base = declarative_base()
 
 
